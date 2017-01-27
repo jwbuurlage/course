b1e75e4d80a92ff2e43e21ca5e49031b43481f12
Jan-Willem Buurlage <janwillembuurlage@gmail.com>
2017-01-27 16:17:52 +0100

    Finish Traversable exercises

--- src/Course/Traversable.hs
+++ src/Course/Traversable.hs
@@ -8,7 +8,7 @@ import Course.Functor
 import Course.Applicative
 import Course.List
 
--- | All instances of the `Traversable` type-class must satisfy two laws. These
+-- | All instances of the `Traversable` type-class must satisfy two (three?) laws. These
 -- laws are not checked by the compiler. These laws are given as:
 --
 -- * The law of naturality
@@ -20,17 +20,21 @@ import Course.List
 -- * The law of composition
 --   `∀f g. traverse ((g <$>) . f) ≅ (traverse g <$>) . traverse f`
 class Functor t => Traversable t where
-  traverse ::
-    Applicative f =>
-    (a -> f b)
-    -> t a
-    -> f (t b)
+  traverse :: Applicative f => (a -> f b) -> t a -> f (t b)
 
 instance Traversable List where
-  traverse ::
-    Applicative f =>
-    (a -> f b)
-    -> List a
-    -> f (List b)
-  traverse f =
-    foldRight (\a b -> (:.) <$> f a <*> b) (pure Nil)
+  traverse :: Applicative f => (a -> f b) -> List a -> f (List b)
+  traverse g xs = foldRight (\a c -> ((:.) <$> g a) <*> c) (pure Nil) xs
+
+  -- foldRight (a -> c -> c) -> c -> [a] -> c
+  -- here a is as a
+  -- while c has type f (List b)
+  -- given an `f List b` and an `a`, obtain an f List b, we have a function from a to f b
+  -- g a = f b
+  -- f b -> List (f b) -> List (f b)
+  --
+  -- OBTAIN: a -> f (List b) -> f (List b)
+  -- HAVE: (:.) :: b -> (List b -> List b)
+  --       (<$>) (:.) :: f b -> f (List b -> List b)
+  --       (<$>) (:.) (g a) :: f (List b -> List b)
+  --       (<*>) (<$>) (:.) (g a) :: f List b -> f List b
