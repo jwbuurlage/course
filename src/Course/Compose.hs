d020e31114f4c9534a5ec27466cd2629ad000829
Jan-Willem Buurlage <janwillembuurlage@gmail.com>
2017-01-27 15:38:51 +0100

    Finish Compose exercises

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -16,20 +16,24 @@ newtype Compose f g a =
 -- Implement a Functor instance for Compose
 instance (Functor f, Functor g) =>
     Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+    --(<$>) :: (a -> b) -> Compose f g a -> Compose f g b
+    f <$> Compose x = Compose $ (<$>) f <$> x
 
 instance (Applicative f, Applicative g) =>
   Applicative (Compose f g) where
 -- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
+-- pure :: a -> Compose f g a
+  pure = Compose . pure . pure
 -- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+-- (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
+  (Compose f) <*> (Compose x) = Compose (lift2 (<*>) f x)
 
 instance (Monad f, Monad g) =>
   Monad (Compose f g) where
 -- Implement the (=<<) function for a Monad instance for Compose
-  (=<<) =
-    error "todo: Course.Compose (<<=)#instance (Compose f g)"
+-- (=<<) :: (a -> Compose f g b) -> Compose f g a -> Compose f g b
+  (=<<) = error "can't be done"
+
+-- I think the problem is that when we use bind inside f, we end up with f a as the second argument,
+-- and we can not extract a out of that, put it in g, and then put the result back in f. And vice versa.
+-- (=<<) :: (a -> (f (g b)) -> (f a) -> f (g b)
